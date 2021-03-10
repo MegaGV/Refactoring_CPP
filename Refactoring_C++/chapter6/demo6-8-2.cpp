c036fbaf822355aead0bb3f0f18703dd5ba45fc6
MegaGV <595654578@qq.com>
2021-03-10 13:27:53 +0800

    fix

--- Refactoring_C++/chapter6/demo6-8-2.cpp
+++ Refactoring_C++/chapter6/demo6-8-2.cpp
@@ -2,39 +2,40 @@
 
 class Account
 {
+private:
+	class Gamma
+	{
+	private:
+		int delta;
+		int inputVal;
+		int quantity;
+		int yearToDate;
+		int importantValue1;
+		int importantValue2;
+		int importantValue3;
+
+		void importantThing() {
+			if ((yearToDate - importantValue1) > 100) {
+				importantValue2 -= 20;
+			}
+		}
+
+	public:
+		Gamma(int inputValArg, int quantityArg, int yearToDateArg, int deltaArg)
+			: inputVal(inputValArg), quantity(quantityArg), yearToDate(yearToDateArg), delta(deltaArg) {}
+		const int compute() {
+			importantValue1 = (inputVal * quantity) + delta;
+			importantValue2 = (inputVal * yearToDate) + 100;
+			importantThing();
+			importantValue3 = importantValue2 * 7;
+			// and so on
+			return importantValue3 - 2 * importantValue1;
+		}
+	};
 public:
 	int delta();
 	int gamma(int inputVal, int quantity, int yearToDate) {
-		return new Gamma(inputVal, quantity, yearToDate, delta())->compute();
+		return Gamma(inputVal, quantity, yearToDate, delta()).compute();
 	}
 };
 
-class Gamma
-{
-private:
-	int delta;
-	int inputVal;
-	int quantity;
-	int yearToDate;
-	int importantValue1;
-	int importantValue2;
-	int importantValue3;
-
-	void importantThing() {
-		if ((yearToDate - importantValue1) > 100) {
-			importantValue2 -= 20;
-		}
-	}
-
-public:
-	Gamma(int inputValArg, int quantityArg, int yearToDateArg, int deltaArg) 
-		: inputVal(inputValArg), quantity(quantityArg), yearToDate(yearToDateArg), delta(deltaArg){}
-	int compute() {
-		importantValue1 = (inputVal * quantity) + delta;
-		importantValue2 = (inputVal * yearToDate) + 100;
-		importantThing();
-		importantValue3 = importantValue2 * 7;
-		// and so on
-		return importantValue3 - 2 * importantValue1;
-	}
-};
